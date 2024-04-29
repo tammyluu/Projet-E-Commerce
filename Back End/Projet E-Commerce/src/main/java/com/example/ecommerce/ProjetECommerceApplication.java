1d94d05b395a05130f3e57a20df3d19a88f6e393
jounetsuST <sanhtam.lam@gmail.com>
2024-04-29 11:18:36 +0200

    back

---
+++ Back End/Projet E-Commerce/src/main/java/com/example/ecommerce/ProjetECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetECommerceApplication.class, args);
+    }
+
+}
