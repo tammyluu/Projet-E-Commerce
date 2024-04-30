e78f5b74d8d32b3ce549d8e69cb1bbae78e866eb
Rayanadmi <rayan.admii@gmail.com>
2024-04-30 14:11:42 +0200

    30 avril début d'apres midi

---
+++ Front end/my-react-app/src/components/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import products from "../bdd/products.json"
+
+function ProductList (){
+
+    return (
+        <>
+            <Header/>
+
+        </>
+    )
+
+}
+export default ProductList
--- Front end/my-react-app/src/main.jsx
+++ Front end/my-react-app/src/main.jsx
@@ -5,12 +5,14 @@ import App from './App.jsx';
 import './index.css';
 import Login from './components/LoginForm.jsx';
 import Register from './components/RegisterForm.jsx';
+import ProductList from './components/ProductList.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path='/login' element={<Login/>} />
+      <Route path='/product' element={<ProductList/>} />
       <Route path='/register' element={<Register/>} />
 
     </Routes>
